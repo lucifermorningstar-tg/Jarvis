b29526990acd88d17121f1fae04e28d08b085bb5
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-18 15:49:32 +0530

    Update nightmode.py

--- Jarvis/modules/nightmode.py
+++ Jarvis/modules/nightmode.py
@@ -1,12 +1,12 @@
 import os
 
-from MashaRoBot.modules.sql_extended.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
+from Jarvis.modules.sql_extended.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
 from telethon.tl.types import ChatBannedRights
 from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
-from MashaRoBot.events import register
-from MashaRoBot import OWNER_ID
-from MashaRoBot import telethn as tbot
+from Jarvis.events import register
+from Jarvis import OWNER_ID
+from Jarvis import telethn as tbot
 from telethon import *
 from telethon import Button, custom, events
 
