6a4ff0542879e5b764aa6410119ddc14fe7690ef
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-23 10:35:04 +0530

    Update karma.py

--- Jarvis/modules/karma.py
+++ Jarvis/modules/karma.py
@@ -1,4 +1,4 @@
-from Jarvis import app
+from Jarvis import pbot
 from Jarvis.utils.errors import capture_err
 from Jarvis.utils.dbfunctions import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
@@ -16,7 +16,7 @@ regex_upvote = r"^((?i)\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|go
 regex_downvote = r"^(\-|\-\-|\-1|👎)$"
 
 
-@app.on_message(filters.text
+@pbot.on_message(filters.text
                 & filters.group
                 & filters.incoming
                 & filters.reply
@@ -47,7 +47,7 @@ async def upvote(_, message):
     )
 
 
-@app.on_message(filters.text
+@pbot.on_message(filters.text
                 & filters.group
                 & filters.incoming
                 & filters.reply
@@ -78,7 +78,7 @@ async def downvote(_, message):
     )
 
 @capture_err
-@app.on_message(filters.command("karma") & filters.group)
+@pbot.on_message(filters.command("karma") & filters.group)
 async def karma(_, message):
     chat_id = message.chat.id
 
@@ -90,7 +90,7 @@ async def karma(_, message):
         for i in karma:
             user_id = await alpha_to_int(i)
             user_karma = karma[i]['karma']
-            user_name = (await app.get_users(user_id)).username
+            user_name = (await pbot.get_users(user_id)).username
             karma_dicc[user_name] = user_karma
 
             karma_arranged = dict(
