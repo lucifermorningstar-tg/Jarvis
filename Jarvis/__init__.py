fea20aa39b8eb8ccf59d335efd21bfd840b2ef23
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-23 08:39:29 +0530

    Update __init__.py

--- Jarvis/__init__.py
+++ Jarvis/__init__.py
@@ -195,6 +195,7 @@ else:
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("jarvis", API_ID, API_HASH)
 pbot = Client("jarvispbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+app = Client("jarvus", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
